132949bb7bf326f08c4b3ee6f993af250ea27fcc
aritbiswas156-cyber <aritbiswas156@gmail.com>
2025-10-17 11:14:25 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+document.addEventListener("DOMContentLoad",()=> {
+	cont loginForm=document.getElementById
+	("loginForm");
+	
+if(loginForm){
+	loginForm.addEventListener ("submit",function(e){
+		e.preventDefault();
+		cont name=document.getElementById("studentName").value;
+		cont id=document.getElementById("studentId").value;
+		cont teacher=document.getElementById("teacherName").value;
+		cont password=document.getElementById("examPassword").value;
+		
+	if(passeord==="exam123"){
+		seassionStorage.setItem("studentName",name);
+		window.location.herf="exam.html";    
+	}	
+	else{
+		alert("Invalid exam Password");
+	}
+	});
+}
+const examForm=document.getElementById("examForm");
+if(examForm){
+	examForm.addEventListener("submit",function(e){
+		e.preventDefault();
+		let.score=0;
+		
+	if(document.querySelector('Input[name="q1"]:checked')?.value==="Paris")score++;
+	if(document.querySelector('Input[name="q2"]:checked')?.value==="java")score++;
+	window.location.href=`result.html?score=${score}`;
+	});
+}
+});
